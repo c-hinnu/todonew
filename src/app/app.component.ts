4b8a5af257bda4b19f18323253c274a64391b783
c-hinnu <235754@ust-global.com>
2022-10-14 14:35:38 +0530

    6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
 
+
+
+
+
 @Component({
+
   selector: 'app-root',
+
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+  styleUrls: ['./app.component.css'],
+
 })
+
 export class AppComponent {
-  title = 'todonew';
+
+  title = 'first_project';
+
+  list:any[]=[];
+
+ 
+
+  EnterSubmit(item:String,bo:any)
+
+    {
+
+     
+
+      if(item!="")
+
+this.list.push({name:item});
+
+bo.value=""
+
+
+
+}
+
+  remove(i:number)
+
+  {
+
+this.list.splice(i,1);
+
+  }
+
+
+
+
 }
